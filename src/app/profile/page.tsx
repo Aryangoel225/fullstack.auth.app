b048ddb7a8f2b7d2b0e988d47e926ff890f33581
Aryan Goel <aryangoel225@gmail.com>
2025-09-20 18:03:55 -0400

    new midleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -44,7 +44,6 @@ export default function ProfilePage() {
         className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >GetUser Details</button>
 
-
-            </div>
+         </div>
     )
 }
